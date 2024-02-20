973f94b9e86ba217d231905f1d8e7ae9526e5ad6
UnknownKoreanSmiley <91345575+UnknownKoreanSmiley@users.noreply.github.com>
2024-02-20 22:34:01 +0530

    add screenshot and some minor changes

--- gemini.py
+++ gemini.py
@@ -1,8 +1,8 @@
 import json
 import os
 import re
-from dotenv import load_dotenv
 import requests
+from dotenv import load_dotenv
 import telebot
 
 load_dotenv()
@@ -11,100 +11,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 GEMINI_TOKEN = os.getenv("GEMINI_TOKEN")
 
 bot = telebot.TeleBot(BOT_TOKEN)
-
-def find_all_index(str, pattern):
-    index_list = [0]
-    for match in re.finditer(pattern, str, re.MULTILINE):
-        if match.group(1) != None:
-            start = match.start(1)
-            end = match.end(1)
-            index_list += [start, end]
-    index_list.append(len(str))
-    return index_list
-
-def replace_all(text, pattern, function):
-    poslist = [0]
-    strlist = []
-    originstr = []
-    poslist = find_all_index(text, pattern)
-    for i in range(1, len(poslist[:-1]), 2):
-        start, end = poslist[i : i + 2]
-        strlist.append(function(text[start:end]))
-    for i in range(0, len(poslist), 2):
-        j, k = poslist[i : i + 2]
-        originstr.append(text[j:k])
-    if len(strlist) < len(originstr):
-        strlist.append("")
-    else:
-        originstr.append("")
-    new_list = [item for pair in zip(originstr, strlist) for item in pair]
-    return "".join(new_list)
-
-def escapeshape(text):
-    return "▎*" + text.split()[1] + "*"
-
-
-def escapeminus(text):
-    return "\\" + text
-
-
-def escapebackquote(text):
-    return r"\`\`"
-
-
-def escapeplus(text):
-    return "\\" + text
-
-def escape(text, flag=0):
-    # In all other places characters
-    # _ * [ ] ( ) ~ ` > # + - = | { } . !
-    # must be escaped with the preceding character '\'.
-    text = re.sub(r"\\\[", "@->@", text)
-    text = re.sub(r"\\\]", "@<-@", text)
-    text = re.sub(r"\\\(", "@-->@", text)
-    text = re.sub(r"\\\)", "@<--@", text)
-    if flag:
-        text = re.sub(r"\\\\", "@@@", text)
-    text = re.sub(r"\\", r"\\\\", text)
-    if flag:
-        text = re.sub(r"\@{3}", r"\\\\", text)
-    text = re.sub(r"_", "\_", text)
-    text = re.sub(r"\*{2}(.*?)\*{2}", "@@@\\1@@@", text)
-    text = re.sub(r"\n{1,2}\*\s", "\n\n• ", text)
-    text = re.sub(r"\*", "\*", text)
-    text = re.sub(r"\@{3}(.*?)\@{3}", "*\\1*", text)
-    text = re.sub(r"\!?\[(.*?)\]\((.*?)\)", "@@@\\1@@@^^^\\2^^^", text)
-    text = re.sub(r"\[", "\[", text)
-    text = re.sub(r"\]", "\]", text)
-    text = re.sub(r"\(", "\(", text)
-    text = re.sub(r"\)", "\)", text)
-    text = re.sub(r"\@\-\>\@", "\[", text)
-    text = re.sub(r"\@\<\-\@", "\]", text)
-    text = re.sub(r"\@\-\-\>\@", "\(", text)
-    text = re.sub(r"\@\<\-\-\@", "\)", text)
-    text = re.sub(r"\@{3}(.*?)\@{3}\^{3}(.*?)\^{3}", "[\\1](\\2)", text)
-    text = re.sub(r"~", "\~", text)
-    text = re.sub(r">", "\>", text)
-    text = replace_all(text, r"(^#+\s.+?$)|```[\D\d\s]+?```", escapeshape)
-    text = re.sub(r"#", "\#", text)
-    text = replace_all(
-        text, r"(\+)|\n[\s]*-\s|```[\D\d\s]+?```|`[\D\d\s]*?`", escapeplus
-    )
-    text = re.sub(r"\n{1,2}(\s*)-\s", "\n\n\\1• ", text)
-    text = re.sub(r"\n{1,2}(\s*\d{1,2}\.\s)", "\n\n\\1", text)
-    text = replace_all(
-        text, r"(-)|\n[\s]*-\s|```[\D\d\s]+?```|`[\D\d\s]*?`", escapeminus
-    )
-    text = re.sub(r"```([\D\d\s]+?)```", "@@@\\1@@@", text)
-    text = replace_all(text, r"(``)", escapebackquote)
-    text = re.sub(r"\@{3}([\D\d\s]+?)\@{3}", "```\\1```", text)
-    text = re.sub(r"=", "\=", text)
-    text = re.sub(r"\|", "\|", text)
-    text = re.sub(r"{", "\{", text)
-    text = re.sub(r"}", "\}", text)
-    text = re.sub(r"\.", "\.", text)
-    text = re.sub(r"!", "\!", text)
-    return text
 
 def load_data(file_name):
     if os.path.exists(file_name):
@@ -137,25 +43,27 @@ def update_json(user_id, role, text):
 
 def generate_content(payload):
     url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={GEMINI_TOKEN}"
-    headers = {
-        'Content-Type': 'application/json'
-    }
+    headers = {'Content-Type': 'application/json'}
     response = requests.post(url, headers=headers, data=json.dumps(payload))
-    print(response.status_code)
     return response.json()
 
 def send_generated_content(user_id, content):
+    formatted_content = ""
     for part in content['parts']:
-        bot.send_message(user_id,escape(part['text'],"MarkdownV2"))
+        cleaned_text = part['text']
+        cleaned_text = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', cleaned_text)
+        cleaned_text = re.sub(r'```(.*?)```', r'<i>\1</i>', cleaned_text)
+        formatted_content += cleaned_text + "\n"
+    bot.send_message(user_id, formatted_content, parse_mode="HTML")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, f"Hello {message.from_user.first_name}!")
 
-@bot.message_handler(func=lambda message: message.text and not message.text.startswith('/'))
+@bot.message_handler(func=lambda message: message.content_type == 'text' and not message.text.startswith('/'))
 def process_message(message):
     user_id = str(message.from_user.id)
-    user_input = message.text
+    user_input = f'{message.text}'
     data = update_json(user_id, "user", user_input)
 
     payload = {"contents": data['contents']}
@@ -166,5 +74,15 @@ def process_message(message):
         if content['content']['role'] == 'model':
             update_json(user_id, "model", content['content']['parts'][0]['text'])
             send_generated_content(user_id, content['content'])
+
+@bot.message_handler(commands=['clear'])
+def clear_history(message):
+    user_id = str(message.from_user.id)
+    file_name = f"{user_id}.json"
+    if os.path.exists(file_name):
+        os.remove(file_name)
+        bot.reply_to(message, "History cleared successfully.")
+    else:
+        bot.reply_to(message, "No history found.")
 
 bot.infinity_polling()
